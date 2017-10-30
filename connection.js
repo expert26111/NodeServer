5801bb9065bb4cba4f3611cdf159e7170810fada
expert26111 <djoana.v@gmail.com>
2017-10-30 11:52:03 +0100

    changed connection.js

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
-    host: '172.17.0.1:3306',
+    host: '172.17.0.1',
     user: 'root',
     password: 'root',
     database: 'hackernews'
