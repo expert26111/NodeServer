fe2d1578c317734c51b277a05d1ebd744eea203e
expert26111 <djoana.v@gmail.com>
2017-11-01 23:42:18 +0100

    host fixed

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
-    host: 'localhost',
+    host: '172.18.0.1"',
     user: 'root',
     password: 'root',
     database: 'hackernews'
