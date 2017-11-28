4493cf6c8edebcf141a5d720a1fa29fbff6573f3
expert26111 <djoana.v@gmail.com>
2017-11-28 16:22:39 +0100

    added 3306

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 //172.18.0.1
 var connection = mysql.createPool({
-    host: 'l72.18.0.1',
+    host: 'l72.18.0.1:3306',
     user: 'root',
     password: 'root',
     database: 'hackernews'
